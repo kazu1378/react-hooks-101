559bfd21197802181a502f0487fd76118dcd0175
kazu1378 <kazua0001@gmail.com>
2021-03-31 19:44:53 +0900

    count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count +1)
+  const decrement = () => setCount(count -1)
+
+  const increment2 = () => setCount(previousCount => previousCount +1)
+  const decrement2 = () => setCount(previousCount => previousCount -1)
+
+  const reset = () => setCount(0)
+  const double = () => setCount(count *2)
+  const tri = () => {if(count%3 == 0){setCount(count/3)}else{return}}
+  console.log(count)
   return (
-    <div>
-      this is a template for react app
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+      <button onClick={reset}>reset</button>
+      </div>
+      <div>
+      <button onClick={double}>*2</button>
+      <button onClick={tri}>3の倍数のとき3で割る</button>
+      </div>
+    </>
   )
 }
 
